b79aa90cac2e25aebe75bb37455e802593831b78
Alamsyah Putra Herdani <aputraherdani@gmail.com>
2021-10-13 08:58:45 +0700

    First Commit

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private Rigidbody2D Rb;
+    public float speed = 400;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.AddForce(new Vector2(20 * Time.deltaTime * speed, 20 * Time.deltaTime * speed));
+    }
+}
